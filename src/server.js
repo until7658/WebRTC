7ef0d8da29d5917bd0e66d79a803046093cc7eb1
until <until7658@gmail.com>
2024-04-06 21:59:41 +0900

    websocket is complete

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import http from "http";
-import WebSocket from "ws";
+import { Server } from "socket.io"
+// import WebSocket from "ws";
 import express from "express";
+import {logPlugin} from "@babel/preset-env/lib/debug";
+import e from "express";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -13,34 +17,88 @@ app.get("/*", (_, res)=>res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-function onSocketClose() {
-    console.log("Disconnected from the Server");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Namsik";
-    console.log("Connected to Browser");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach(aSocket => {
-                    aSocket.send(`${socket.nickname}: ${message.payload}`);
-                });
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break;
-        }
-        console.log(message);
-    });
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true
+    }
 });
 
-server.listen(3000, handleListen);
+instrument(wsServer, {
+    auth: false,
+});
+
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms }
+        }
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+wsServer.on("connection", (socket)=>{
+    socket["nickname"] = "Namsik";
+    socket.onAny((event)=>{
+        // console.log(wsServer.sockets.adapter);
+        console.log(`Socket Event: ${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) =>
+            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+        );
+    });
+    socket.on("disconnect",() =>{
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+});
+
+// const wss = new WebSocket.Server({ server });
+// function onSocketClose() {
+//     console.log("Disconnected from the Server");
+// }
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Namsik";
+//     console.log("Connected to Browser");
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//             case "new_message":
+//                 sockets.forEach(aSocket => {
+//                     aSocket.send(`${socket.nickname}: ${message.payload}`);
+//                 });
+//                 break;
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//                 break;
+//         }
+//         console.log(message);
+//     });
+// });
+
+httpServer.listen(3000, handleListen);
